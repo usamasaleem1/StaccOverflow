0d9904c711c53db4e56ea6af453ec32251cb0d41
Shadi Jiha <shadosite@gmail.com>
2021-10-03 01:04:46 -0400

    Added register method in AuthController #7
    Also added the required routes. I haven't tested these though because we need the frontend for that

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $email    = $request->get("email");
+        $password = $request->get("password");
+        $name     = $request->get("username");
+
+        // Validate the input
+        $validator = Validator::make($request->all(), [
+            "email" => "required|email|unique:users",
+            "name"  => "required|max:100"
+        ]);
+
+        // If the validator fails
+        if ($validator->fails()) {
+            return redirect('register')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        // Otherwise it is ok, all user to database
+        $user           = new User();
+        $user->email    = $email;
+        $user->password = Hash::make($password);
+        $user->name     = $name;
+        $user->save();
+
+        Auth::setUser($user);
+
+        return view("index");
+    }
+
+}
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth routes
+Route::get('/register', function () {
+    return view("register");
+});
+Route::post("/register", [AuthController::class, "register"]);
