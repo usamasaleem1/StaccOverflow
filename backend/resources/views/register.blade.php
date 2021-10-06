368e70ac57260e3b020247e0607b8e795456c50a
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-10-06 12:52:15 -0400

    Register page backend complete

--- backend/resources/views/register.blade.php
+++ backend/resources/views/register.blade.php
@@ -1,12 +1,29 @@
-@extends("layouts.main")
-@section("content")
+<html>
 
-    <form action="/register" method="POST">
-        @csrf
-        <input type="email" name="email" placeholder="Email"/>
-        <input type="text" name="name" placeholder="Name"/>
-        <input type="password" name="password" placeholder="Password"/>
-        <input type="submit" value="Register"/>
-    </form>
+<head>
+  <link rel="stylesheet" href="css/signup.css">
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  <title>Sign in</title>
+</head>
 
-@endsection
+<body>
+  <div class="main">
+    <p class="sign" Align="center">Sign Up</p>
+    <p class="title" Align="center">Make an Account and Start Asking</p>
+    <img src="https://static.thenounproject.com/png/538846-200.png" alt="User Icon" width="50" class="center"/>
+    <form class="form1">
+      <input class="un " type="text" Align="center" placeholder="Email">
+      <input class="un " type="text" Align="center" placeholder="Username">
+      <input class="pass" type="password" Align="center" placeholder="Password">
+      <input class="pass" type="password" Align="center" placeholder="Confirm Password">
+      <a class="submit" a href="register">Sign up</a>
+      <p class="forgot" Align="center"><a href="signin">Already have an account? Sign in here</p>
+            
+                
+    </div>
+     
+</body>
+
+</html>
