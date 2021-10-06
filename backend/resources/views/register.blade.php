b0a2614999a7aef0987146efad3ad330330b3eb5
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-10-06 13:02:20 -0400

    Update register.blade.php

--- backend/resources/views/register.blade.php
+++ backend/resources/views/register.blade.php
@@ -5,7 +5,7 @@
   <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
-  <title>Sign in</title>
+  <title>Sign Up</title>
 </head>
 
 <body>
