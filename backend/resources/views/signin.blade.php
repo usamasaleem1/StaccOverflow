653392d84795256cba3a1dcf04b7e5afea107824
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-10-05 20:13:16 -0400

    Update signin.blade.php

--- backend/resources/views/signin.blade.php
+++ backend/resources/views/signin.blade.php
@@ -1,38 +1,12 @@
+@extends("layouts.main")
+@section("content")
 
-<!DOCTYPE html>
-<html>
-<head>
-<link href="{{ asset('public/css/signin.css') }}" rel="stylesheet">
-</head>
-<body>
-
-<div class="wrapper fadeInDown">
-  <div id="formContent">
-    <!-- Tabs Titles -->
-    <h2 class="active"> Sign In </h2>
-    <h2 class="inactive underlineHover">Sign Up </h2>
-
-    <!-- Icon -->
-    <div class="fadeIn first">
-      <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
-    </div>
-
-    <!-- Login Form -->
-    <form>
-      <input type="text" id="login" class="fadeIn second" name="login" placeholder="login">
-      <input type="text" id="password" class="fadeIn third" name="login" placeholder="password">
-      <input type="submit" class="fadeIn fourth" value="Log In">
+    <form action="/register" method="POST">
+        @csrf
+        <input type="email" name="email" placeholder="Email"/>
+        <input type="text" name="name" placeholder="Name"/>
+        <input type="password" name="password" placeholder="Password"/>
+        <input type="submit" value="Register"/>
     </form>
-
-    <!-- Remind Passowrd -->
-    <div id="formFooter">
-      <a class="underlineHover" href="#">Forgot Password?</a>
-    </div>
-
-  </div>
-</div>
-
-</body>
-</html>
 
 @endsection
