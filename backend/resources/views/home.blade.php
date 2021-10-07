c83cf7b8464f82530202393532a8a9420d5c287e
Usama Saleem <50121574+usamasaleem1@users.noreply.github.com>
2021-10-07 17:50:36 -0400

    FE - Question Card Layout Finished
    FrontEnd

--- backend/resources/views/home.blade.php
+++ backend/resources/views/home.blade.php
@@ -1,3 +1,4 @@
+
 @extends("layouts.withHeader")
 
 @section("title")
@@ -16,6 +17,23 @@
                         <u>Team at SOEN 341</u></a>
                 </p>
             </header>
+
+            <div class="card-container">
+              <div class="card">
+                <div class="img">
+                </div>
+                    <h2 style="margin: 25px";>Question Title</h2>
+                    <p style="margin-left: 30px";>Here's what I mean, for example with Grover's search algorithm we are interested in 
+                        isolating a single state from a superposition of multiple states, from the video it seems to me we are interested 
+                        in isolating multiple states from a superposition of multiple states. Since qubits collapse only to a single state 
+                        after measurement, How would measurement here yield multiple states or is there a work around? </p>
+
+            </div>
+
+
+
+
+
             <div class="py-5">
                 <p class="lead">Lorem ipsum dolor sit amet, <strong class="font-weight-bold">consectetur
                         adipisicing </strong>elit. Explicabo consectetur odio voluptatum facere animi temporibus,
@@ -31,21 +49,13 @@
                     culpa
                     qui officia deserunt mollit anim id est laborum.</p>
             </div>
-            <div class="py-5">
-                <p class="lead">Lorem ipsum dolor sit amet, <strong class="font-weight-bold">consectetur
-                        adipisicing </strong>elit. Explicabo consectetur odio voluptatum facere animi temporibus,
-                    distinctio
-                    tempore enim corporis quam <strong class="font-weight-bold">recusandae </strong>placeat! Voluptatum
-                    voluptate, ex modi illum quas nam distinctio.</p>
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                    incididunt
-                    ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa
-                    qui officia deserunt mollit anim id est laborum.</p>
-            </div>
+
+
+
+
         </div>
     </div>
 @endsection
+
+
+
---
+++ backend/resources/views/layouts/homeQuesTemplate.blade.php
@@ -0,0 +1,8 @@
+
+@section("title")
+Home Page
+@endsection
+
+@section("styles")
+<link rel="stylesheet" href="css/signin.css">
+@endsection
